5501de09a235043ea25cfa49960671bf2a2a6028
kao1531 <allisonkao8@gmail.com>
2019-08-24 17:55:36 -0500

    final version

--- TipCalculator/app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ TipCalculator/app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.tipcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DecimalFormat;
 
@@ -25,14 +27,35 @@ public class MainActivity extends AppCompatActivity {
         TextView finalTextView = (TextView) findViewById(R.id.finalAmountTextView);
         TextView tipTextView = (TextView) findViewById(R.id.tipTextView);
 
-        double bill = Double.parseDouble(billCostEditText.getText().toString());
-        double percent = Double.parseDouble(tipPercentEditText.getText().toString());
+        double bill = 0, percent = 0;
+        Context context = getApplicationContext();
+        CharSequence text = "Please enter a bill total and tip percent";
+        int duration = Toast.LENGTH_LONG;
 
-        double tip = bill * (percent/100.0);
-        double total = tip + bill;
+        Toast toast = Toast.makeText(context, text, duration);
 
-        finalTextView.setText("Final Bill Amount: " + df.format(total));
-        tipTextView.setText("Tip Amount: " + df.format(tip));
+        boolean valid = false;
+        while (valid == false)
+        {
+           try
+           {
+               bill = Double.parseDouble(billCostEditText.getText().toString());
+               percent = Double.parseDouble(tipPercentEditText.getText().toString());
+               valid = true;
+               double tip = bill * (percent/100.0);
+               double total = tip + bill;
+
+               finalTextView.setText("Final Bill Amount: " + df.format(total));
+               tipTextView.setText("Tip Amount: " + df.format(tip));
+
+           }
+
+           catch (Exception e)
+            {
+                toast.show();
+                valid = true;
+            }
+        }
     }
 
     public void happyTip (View v)
@@ -42,15 +65,35 @@ public class MainActivity extends AppCompatActivity {
         EditText tipPercentEditText = (EditText) findViewById(R.id.tipPercentEditText);
         TextView finalTextView = (TextView) findViewById(R.id.finalAmountTextView);
         TextView tipTextView = (TextView) findViewById(R.id.tipTextView);
+        double bill = 0;
 
-        double bill = Double.parseDouble(billCostEditText.getText().toString());
+        Context context = getApplicationContext();
+        CharSequence text = "Please enter a bill total";
+        int duration = Toast.LENGTH_LONG;
 
-        double tip = bill * 0.20;
-        double total = bill + tip;
+        Toast toast = Toast.makeText(context, text, duration);
 
-        finalTextView.setText("Final Bill Amount: " + df.format(total));
-        tipTextView.setText("Tip Amount: " + df.format(tip));
-        tipPercentEditText.setText("20");
+        boolean valid = false;
+        while (valid == false)
+        {
+            try
+            {
+                bill = Double.parseDouble(billCostEditText.getText().toString());
+                valid = true;
+                double tip = bill * .20;
+                double total = tip + bill;
+
+                finalTextView.setText("Final Bill Amount: " + df.format(total));
+                tipTextView.setText("Tip Amount: " + df.format(tip));
+                tipPercentEditText.setText("20");
+            }
+
+            catch (Exception e)
+            {
+                toast.show();
+                valid = true;
+            }
+        }
     }
 
     public void neutralTip (View v)
@@ -61,14 +104,35 @@ public class MainActivity extends AppCompatActivity {
         TextView finalTextView = (TextView) findViewById(R.id.finalAmountTextView);
         TextView tipTextView = (TextView) findViewById(R.id.tipTextView);
 
-        double bill = Double.parseDouble(billCostEditText.getText().toString());
+        double bill = 0;
 
-        double tip = bill * 0.15;
-        double total = bill + tip;
+        Context context = getApplicationContext();
+        CharSequence text = "Please enter a bill total";
+        int duration = Toast.LENGTH_LONG;
 
-        finalTextView.setText("Final Bill Amount: " + df.format(total));
-        tipTextView.setText("Tip Amount: " + df.format(tip));
-        tipPercentEditText.setText("15");
+        Toast toast = Toast.makeText(context, text, duration);
+
+        boolean valid = false;
+        while (valid == false)
+        {
+            try
+            {
+                bill = Double.parseDouble(billCostEditText.getText().toString());
+                valid = true;
+                double tip = bill * .15;
+                double total = tip + bill;
+
+                finalTextView.setText("Final Bill Amount: " + df.format(total));
+                tipTextView.setText("Tip Amount: " + df.format(tip));
+                tipPercentEditText.setText("15");
+            }
+
+            catch (Exception e)
+            {
+                toast.show();
+                valid = true;
+            }
+        }
     }
 
     public void sadTip (View v)
@@ -79,13 +143,34 @@ public class MainActivity extends AppCompatActivity {
         TextView finalTextView = (TextView) findViewById(R.id.finalAmountTextView);
         TextView tipTextView = (TextView) findViewById(R.id.tipTextView);
 
-        double bill = Double.parseDouble(billCostEditText.getText().toString());
+        double bill = 0;
 
-        double tip = bill * 0.10;
-        double total = bill + tip;
+        Context context = getApplicationContext();
+        CharSequence text = "Please enter a bill total";
+        int duration = Toast.LENGTH_LONG;
 
-        finalTextView.setText("Final Bill Amount: " + df.format(total));
-        tipTextView.setText("Tip Amount: " + df.format(tip));
-        tipPercentEditText.setText("10");
+        Toast toast = Toast.makeText(context, text, duration);
+
+        boolean valid = false;
+        while (valid == false)
+        {
+            try
+            {
+                bill = Double.parseDouble(billCostEditText.getText().toString());
+                valid = true;
+                double tip = bill * .10;
+                double total = tip + bill;
+
+                finalTextView.setText("Final Bill Amount: " + df.format(total));
+                tipTextView.setText("Tip Amount: " + df.format(tip));
+                tipPercentEditText.setText("10");
+            }
+
+            catch (Exception e)
+            {
+                toast.show();
+                valid = true;
+            }
+        }
     }
 }
